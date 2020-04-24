e074b96ee22368fb874fb65e7778d7c053ac4c90
TA20015067 <tanuja.bameta@wipro.com>
2020-04-24 10:44:59 +0530

    Updates

---
+++ precache-manifest.a9cbba83394ecaff47eb065bdf7c8413.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb36f84314ed562c16fb7b5ce107b2a4",
+    "url": "/FactsVendor/index.html"
+  },
+  {
+    "revision": "d49f11d7c5c696fd541d",
+    "url": "/FactsVendor/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "736c021616940e9fe0b2",
+    "url": "/FactsVendor/static/css/main.3a029c4b.chunk.css"
+  },
+  {
+    "revision": "d49f11d7c5c696fd541d",
+    "url": "/FactsVendor/static/js/2.21f260ae.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/FactsVendor/static/js/2.21f260ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "736c021616940e9fe0b2",
+    "url": "/FactsVendor/static/js/main.4cde084b.chunk.js"
+  },
+  {
+    "revision": "f2d8b5c350e6c87e32c5",
+    "url": "/FactsVendor/static/js/runtime-main.f484be38.js"
+  }
+]);
